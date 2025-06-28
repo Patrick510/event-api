d6d73aa5bce5c863a5c9ee10faa337ba1a071efc
Patrick510 <patrickdedinho321xz@gmail.com>
2025-06-28 12:42:24 -0400

    Add RabbitMQ configuration for email queue and message conversion

--- src/main/java/com/ms/events/configuration/RabbitMQConfig.java
+++ src/main/java/com/ms/events/configuration/RabbitMQConfig.java
@@ -1,6 +1,9 @@
 package com.ms.events.configuration;
 
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,5 +15,18 @@ public class RabbitMQConfig {
   @Bean
   public Queue queue() {
     return new Queue(QUEUE_NAME, false);
+  }
+
+  @Bean
+  public Jackson2JsonMessageConverter messageConverter() {
+    return new Jackson2JsonMessageConverter();
+  }
+
+  @Bean
+  public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory,
+      Jackson2JsonMessageConverter messageConverter) {
+    RabbitTemplate template = new RabbitTemplate(connectionFactory);
+    template.setMessageConverter(messageConverter);
+    return template;
   }
 }
